3c5448909d127f1caf132e254fd3c272a34b6593
Omillo Fidel Charles <fidelomillo812@gmail.com>
2025-12-06 23:19:49 +0300

    datatypes

---
+++ basics/dataTypes.cpp
@@ -0,0 +1,8 @@
+// BASIC C++ DATA TYPES
+// Reference: Check out the variables.cpp file for more details on variable declaration and initialization.
+
+// BOOLEAN -Stores true or false values
+// CHAR - Stores a single character/letter/number, or ASCII values
+// INT - Stores whole numbers, without decimals
+// FLOAT - Stores fractional numbers, containing one or more decimals. Sufficient for storing 6-7 decimal digits
+// DOUBLE - Stores fractional numbers, containing one or more decimals. Sufficient for storing 15 decimal digits
